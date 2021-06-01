978141b62d825e4986b32972970d75cdb79da292
PigihunterJesh <pigihunter@gmail.com>
2021-05-31 19:02:16 -0500

    Clase Arma

---
+++ Arma.h
@@ -0,0 +1,50 @@
+#ifndef Arma_h
+#define Arma_h
+#include "Item.h"
+
+class Arma: public Item{
+    public: 
+    // Constructores
+    Arma();
+    Arma(int);
+
+    // Getters y Setters
+    int getDanio() const;
+    void setDanio(int);
+
+    // Métodos
+    void usarArma();
+
+    private:
+    int danio;
+};
+
+// Constructores
+Arma :: Arma(){
+    danio = 0;
+}
+
+Arma :: Arma(int dan){
+    danio = dan;
+}
+
+// Getters y Setters
+int Arma :: getDanio() const{return danio;}
+void Arma :: setDanio(int dan){danio = dan;}
+
+// Métodos
+void Arma :: usarArma(){
+    // Cada vez que el usario use un arma, este le restara al número de usos.
+    int cantUsos = Item::getNumUsos();
+    cantUsos -= 1;
+    Item::setNumUsos(cantUsos);
+    // Tendrá que realizar daño a un personaje
+    //
+    //
+    if(cantUsos <= 0){
+        // Destruir Item
+        Item::~Item();
+    }
+}
+
+#endif
