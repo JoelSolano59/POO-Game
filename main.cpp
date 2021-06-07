d8192d11dc1af44eb9891c0fb23a9c6fd67726be
PigihunterJesh <pigihunter@gmail.com>
2021-06-07 18:03:49 -0500

    Agrega la portada y el Ajuste de la pantalla

--- main.cpp
+++ main.cpp
@@ -20,6 +20,8 @@ Fecha de última actualización:
 // Librerías
 #include <iostream>
 #include <cstdlib>
+#include <windows.h>
+#include <fstream>
 using namespace std;
 // Clases hijas que agregan a las clases padres
 #include "Arma.h"
@@ -28,6 +30,46 @@ using namespace std;
 #include "Vacuna.h"
 #include "Inventario.h"
 #include "Personaje.h"
+
+/*
+La primera función que se llama al momento de correr el juego.
+Este avisa al usuario de como debe de quedar la pantalla.
+*/
+void AjustePantalla(){
+    system("cls");
+    ifstream archivo;
+    string linea;
+    archivo.open("AjustePantalla.txt");
+    while(getline(archivo, linea)){
+        cout << linea << endl;
+    }
+    system("Pause");
+    system("cls");
+}
+
+
+/*
+Función que carga la portada del juego, este es llamado después 
+de la función AjustePantalla()
+*/
+void cargarPortada(){
+    ifstream archivo;
+    string linea;
+    archivo.open("Portada.txt");
+
+    for(int i=1; i<26; i++){
+        getline(archivo, linea);
+        if(i<10){
+            cout << linea << endl;
+            Sleep(350);
+        } else{
+            cout << linea << endl;
+            Sleep(50);
+        }
+    }
+    system("Pause");
+    system("cls");
+}
 
 int main() {
     system("Title Time Paradox");
